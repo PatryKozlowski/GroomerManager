3d8f457227671eac7a2b6cdec948c570deb8dc9c
PatryKozlowski <patrykozlowski0@gmail.com>
2025-01-19 19:24:13 +0100

    Added cors

--- backend/GroomerManager.API/Program.cs
+++ backend/GroomerManager.API/Program.cs
@@ -4,6 +4,7 @@ using GroomerManager.Application;
 using GroomerManager.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+const string CORS_POLICY = "GroomeManagerPolicy";
 
 if (builder.Environment.IsDevelopment())
 {
@@ -17,8 +18,20 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAuth(builder.Configuration);
 builder.Services.AddHttpContextAccessor(); 
 builder.Services.AddControllers();
+builder.Services.AddCors(cors => cors.AddPolicy(CORS_POLICY, policy =>
+{
+    policy.WithOrigins(builder.Configuration.GetValue<string>("frontend") ?? throw new InvalidOperationException("frontend url should be provided"));
+    policy.WithMethods("POST", "PUT", "PATCH", "DELETE");
+    policy.WithHeaders("content-type");
+    policy.WithHeaders("X-XSRF-TOKEN");
+    // policy.WithHeaders("Bearer")
+    // policy.AllowAnyHeader();
+    policy.AllowCredentials();
+}));
 
 var app = builder.Build();
+
+app.UseCors(CORS_POLICY);
 
 app.UseExceptionHandler(_ => { });
 
